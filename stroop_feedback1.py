c5b6e620544ca23d720b69857594eeea9670db03
Agata <ag.kpp.37@gmail.com>
2018-04-13 12:20:34 +0200

    DZIAAAAAAAAAAAŁAAAAA!

--- stroop_feedback1.py
+++ stroop_feedback1.py
@@ -1,64 +1,59 @@
-
 from __future__ import absolute_import, division
 from psychopy import locale_setup, sound, gui, visual, core, data, event, logging, clock
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
                                 STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
-import numpy as np  
+import numpy as np  # whole numpy lib is available, prepend 'np.'
 from numpy import (sin, cos, tan, log, log10, pi, average,
                    sqrt, std, deg2rad, rad2deg, linspace, asarray)
 from numpy.random import random, randint, normal, shuffle
-import os  
-import sys  
+import os  # handy system and path functions
+import sys  # to get file system encoding
 
-
+# Ensure that relative paths start from the same directory as this script
 _thisDir = os.path.dirname(os.path.abspath(__file__)).decode(sys.getfilesystemencoding())
 os.chdir(_thisDir)
 
-# to nie wiem czy się przyda, ale w badaniach by się przydało
-zbiera dane o ludziach 
-expName = 'stroop_feedback'  
+# Store info about the experiment session
+expName = 'stroop_feedback'  # from the Builder filename that created this script
 expInfo = {'session': '01', 'participant': ''}
 dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
 if dlg.OK == False:
-    core.quit()  
-expInfo['date'] = data.getDateStr() 
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
 expInfo['expName'] = expName
 
-
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
 filename = _thisDir + os.sep + u'data' + os.sep + '%s_%s' % (expInfo['participant'], expInfo['date'])
 
-
+# An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
     originPath=None,
     savePickle=True, saveWideText=True,
     dataFileName=filename)
-
+# save a log file for detail verbose info
 logFile = logging.LogFile(filename+'.log', level=logging.WARNING)
-logging.console.setLevel(logging.WARNING)  
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
 
-endExpNow = False  
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
 
+# Start Code - component code to be run before the window creation
 
-# wygląd okna, można sb zmienić rozdzielczość, as u wish
-
+# Setup the Window
 win = visual.Window(
     size=[2560, 1440], fullscr=True, screen=0,
     allowGUI=False, allowStencil=False,
     monitor='testMonitor', color='black', colorSpace='rgb',
     blendMode='avg', useFBO=True,
     units='norm')
-
-
+# store frame rate of monitor if we can measure it
 expInfo['frameRate'] = win.getActualFrameRate()
 if expInfo['frameRate'] != None:
     frameDur = 1.0 / round(expInfo['frameRate'])
 else:
-    frameDur = 1.0 / 60.0   #chyba?
+    frameDur = 1.0 / 60.0  # could not measure, so guess
 
-# To są dane do routin pt "Start"
-#TU jest napisany wstępik co to je stroop
-
+# Initialize components for Routine "start"
 startClock = core.Clock()
 text = visual.TextStim(win=win, name='text',
     text=u'Za chwil\u0119 przyst\u0105pisz do udzia\u0142u w tzw. te\u015bcie Stroopa!\n\nTest Ridleya Stroopa s\u0142u\u017cy do mierzenia funkcji poznawczych cz\u0142owieka, koncentracji i podzielno\u015bci uwagi.\n\nTwoim zadaniem b\u0119dzie okre\u015blenie koloru czcionki, kt\xf3r\u0105 zapisane jest s\u0142owo.\nJak b\u0119dziesz gotowy/a wci\u015bnij dowolny przycisk, aby zobaczy\u0107 intrukcje.',
@@ -74,8 +69,7 @@ image = visual.ImageStim(
     flipHoriz=False, flipVert=False,
     texRes=128, interpolate=True, depth=-1.0)
 
-# To są dane do instrukcji
-#przed próbą
+# Initialize components for Routine "instructPractice"
 instructPracticeClock = core.Clock()
 instr1 = visual.TextStim(win=win, name='instr1',
     text=u'Okej, no to najpierw ma\u0142a pr\xf3ba.\n\nWybierasz kolor liter, olewasz s\u0142owa.\n\nlewa strza\u0142ka - czerwony\ndolna strza\u0142ka - zielony\nprawa strza\u0142ka - niebieski\n\nNaci\u015bnij dowolony przycisk aby rozpocz\u0105\u0107',
@@ -84,8 +78,7 @@ instr1 = visual.TextStim(win=win, name='instr1',
     color='White', colorSpace='rgb', opacity=1,
     depth=0.0);
 
-# A tu jest zadanie właściwe
-
+# Initialize components for Routine "trial"
 trialClock = core.Clock()
 word = visual.TextStim(win=win, name='word',
     text='default text',
@@ -100,7 +93,7 @@ text_2 = visual.TextStim(win=win, name='text_2',
     color='White', colorSpace='rgb', opacity=1,
     depth=-2.0);
 
-# Dane to feedbacku który jest po kazdym trialu
+# Initialize components for Routine "feedback"
 feedbackClock = core.Clock()
 msg=''
 feedback_2 = visual.TextStim(win=win, name='feedback_2',
@@ -110,8 +103,7 @@ feedback_2 = visual.TextStim(win=win, name='feedback_2',
     color='White', colorSpace='rgb', opacity=1,
     depth=-1.0);
 
-# Intrukcje 2 - te są przed właściwym zadaniem
-
+# Initialize components for Routine "instruct"
 instructClock = core.Clock()
 instrText = visual.TextStim(win=win, name='instrText',
     text=u'Ok, teraz zaczynamy naprawd\u0119,\npami\u0119taj:\n\nlewa strza\u0142ka - czerwony\ndolna strza\u0142ka - zielony\nprawa strza\u0142ka - niebieski\n\nNaci\u015bnij dowolony przycisk aby rozpocz\u0105\u0107\n',
@@ -120,7 +112,7 @@ instrText = visual.TextStim(win=win, name='instrText',
     color=[1, 1, 1], colorSpace='rgb', opacity=1,
     depth=0.0);
 
-#zadanie
+# Initialize components for Routine "trial"
 trialClock = core.Clock()
 word = visual.TextStim(win=win, name='word',
     text='default text',
@@ -135,17 +127,17 @@ text_2 = visual.TextStim(win=win, name='text_2',
     color='White', colorSpace='rgb', opacity=1,
     depth=-2.0);
 
-#feedback
-feedbackClock = core.Clock()
+# Initialize components for Routine "feedback_koncowy"
+feedback_koncowyClock = core.Clock()
 msg=''
-feedback_2 = visual.TextStim(win=win, name='feedback_2',
+text_3 = visual.TextStim(win=win, name='text_3',
     text='default text',
     font='Times New Roman',
-    pos=[0, 0], height=0.1, wrapWidth=None, ori=0, 
-    color='White', colorSpace='rgb', opacity=1,
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1,
     depth=-1.0);
 
-# dane to ostatniego "slajdu"
+# Initialize components for Routine "thanks"
 thanksClock = core.Clock()
 thanksText = visual.TextStim(win=win, name='thanksText',
     text=u'Ok, to ju\u017c koniec,\ndzi\u0119ki za udzia\u0142 w eksperymencie :)\n\n\nNaci\u015bnij esc aby wyj\u015b\u0107\n',
@@ -154,45 +146,45 @@ thanksText = visual.TextStim(win=win, name='thanksText',
     color='Black', colorSpace='rgb', opacity=1,
     depth=0.0);
 
-globalClock = core.Clock()  
-routineTimer = core.CountdownTimer()  
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
 
-
-
+# ------Prepare to start Routine "start"-------
 t = 0
-startClock.reset()  
+startClock.reset()  # clock
 frameN = -1
 continueRoutine = True
-
+# update component parameters for each repeat
 key_resp_2 = event.BuilderKeyResponse()
-
+# keep track of which components have finished
 startComponents = [text, image, key_resp_2]
 for thisComponent in startComponents:
     if hasattr(thisComponent, 'status'):
         thisComponent.status = NOT_STARTED
 
-#tu się zaczyna wszystko
-
+# -------Start Routine "start"-------
 while continueRoutine:
-    
+    # get current time
     t = startClock.getTime()
-    frameN = frameN + 1 
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
     
-    
+    # *text* updates
     if t >= 0.0 and text.status == NOT_STARTED:
-    
+        # keep track of start time/frame for later
         text.tStart = t
-        text.frameNStart = frameN 
+        text.frameNStart = frameN  # exact frame index
         text.setAutoDraw(True)
     
-   
+    # *image* updates
     if t >= 0.0 and image.status == NOT_STARTED:
-      
+        # keep track of start time/frame for later
         image.tStart = t
-        image.frameNStart = frameN  
+        image.frameNStart = frameN  # exact frame index
         image.setAutoDraw(True)
     
-    # *key_resp_2*
+    # *key_resp_2* updates
     if t >= 0.0 and key_resp_2.status == NOT_STARTED:
         # keep track of start time/frame for later
         key_resp_2.tStart = t
@@ -361,7 +353,7 @@ for thisPractice in practice:
             word.frameNStart = frameN  # exact frame index
             word.setAutoDraw(True)
         
-      
+        # *resp* updates
         if t >= 0.5 and resp.status == NOT_STARTED:
             # keep track of start time/frame for later
             resp.tStart = t
@@ -689,48 +681,78 @@ for thisTrial in trials:
         trials.addData('resp.rt', resp.rt)
     # the Routine "trial" was not non-slip safe, so reset the non-slip timer
     routineTimer.reset()
+    thisExp.nextEntry()
     
-    # ------Prepare to start Routine "feedback"-------
+# completed 3.0 repeats of 'trials'
+
+# get names of stimulus parameters
+if trials.trialList in ([], [None], None):
+    params = []
+else:
+    params = trials.trialList[0].keys()
+# save data for this loop
+trials.saveAsExcel(filename + '.xlsx', sheetName='trials',
+    stimOut=params,
+    dataOut=['n','all_mean','all_std', 'all_raw'])
+
+# set up handler to look after randomisation of conditions etc
+trials_2 = data.TrialHandler(nReps=1, method='random', 
+    extraInfo=expInfo, originPath=-1,
+    trialList=data.importConditions('trialTypes.xlsx'),
+    seed=None, name='trials_2')
+thisExp.addLoop(trials_2)  # add the loop to the experiment
+thisTrial_2 = trials_2.trialList[0]  # so we can initialise stimuli with some values
+# abbreviate parameter names if possible (e.g. rgb = thisTrial_2.rgb)
+if thisTrial_2 != None:
+    for paramName in thisTrial_2:
+        exec('{} = thisTrial_2[paramName]'.format(paramName))
+
+for thisTrial_2 in trials_2:
+    currentLoop = trials_2
+    # abbreviate parameter names if possible (e.g. rgb = thisTrial_2.rgb)
+    if thisTrial_2 != None:
+        for paramName in thisTrial_2:
+            exec('{} = thisTrial_2[paramName]'.format(paramName))
+    
+    # ------Prepare to start Routine "feedback_koncowy"-------
     t = 0
-    feedbackClock.reset()  # clock
+    feedback_koncowyClock.reset()  # clock
     frameN = -1
     continueRoutine = True
-    routineTimer.add(1.000000)
     # update component parameters for each repeat
-    if resp.corr:#stored on last run routine
-      msg="Dobrze! czas reakcji=%.3f" %(resp.rt)
-    else:
-      msg="Ups, zla odpowiedz"
-    feedback_2.setText(msg)
+    meanRt = trials.data['resp.rt'].mean()
+    nCorr = trials.data['resp.corr'].sum()
+    msg = "Ilosc poprawnych odpowiedzi: %i/36     Twoj sredni czas reakcji=%.2f sek" %(nCorr, meanRt)
     # keep track of which components have finished
-    feedbackComponents = [feedback_2]
-    for thisComponent in feedbackComponents:
+    feedback_koncowyComponents = [text_3]
+    for thisComponent in feedback_koncowyComponents:
         if hasattr(thisComponent, 'status'):
             thisComponent.status = NOT_STARTED
     
-    # -------Start Routine "feedback"-------
-    while continueRoutine and routineTimer.getTime() > 0:
+    # -------Start Routine "feedback_koncowy"-------
+    while continueRoutine:
         # get current time
-        t = feedbackClock.getTime()
+        t = feedback_koncowyClock.getTime()
         frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
         # update/draw components on each frame
         
         
-        # *feedback_2* updates
-        if t >= 0.0 and feedback_2.status == NOT_STARTED:
+        # *text_3* updates
+        if t >= 0.0 and text_3.status == NOT_STARTED:
             # keep track of start time/frame for later
-            feedback_2.tStart = t
-            feedback_2.frameNStart = frameN  # exact frame index
-            feedback_2.setAutoDraw(True)
-        frameRemains = 0.0 + 1.0- win.monitorFramePeriod * 0.75  # most of one frame period left
-        if feedback_2.status == STARTED and t >= frameRemains:
-            feedback_2.setAutoDraw(False)
+            text_3.tStart = t
+            text_3.frameNStart = frameN  # exact frame index
+            text_3.setAutoDraw(True)
+        if text_3.status == STARTED:  # only update if drawing
+            text_3.setText(msg
+
+, log=False)
         
         # check if all components have finished
         if not continueRoutine:  # a component has requested a forced-end of Routine
             break
         continueRoutine = False  # will revert to True if at least one component still running
-        for thisComponent in feedbackComponents:
+        for thisComponent in feedback_koncowyComponents:
             if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
                 continueRoutine = True
                 break  # at least one component has not yet finished
@@ -743,22 +765,24 @@ for thisTrial in trials:
         if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
             win.flip()
     
-    # -------Ending Routine "feedback"-------
-    for thisComponent in feedbackComponents:
+    # -------Ending Routine "feedback_koncowy"-------
+    for thisComponent in feedback_koncowyComponents:
         if hasattr(thisComponent, "setAutoDraw"):
             thisComponent.setAutoDraw(False)
     
+    # the Routine "feedback_koncowy" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset()
     thisExp.nextEntry()
     
-# completed 3.0 repeats of 'trials'
+# completed 1 repeats of 'trials_2'
 
 # get names of stimulus parameters
-if trials.trialList in ([], [None], None):
+if trials_2.trialList in ([], [None], None):
     params = []
 else:
-    params = trials.trialList[0].keys()
+    params = trials_2.trialList[0].keys()
 # save data for this loop
-trials.saveAsExcel(filename + '.xlsx', sheetName='trials',
+trials_2.saveAsExcel(filename + '.xlsx', sheetName='trials_2',
     stimOut=params,
     dataOut=['n','all_mean','all_std', 'all_raw'])
 
@@ -804,18 +828,6 @@ while continueRoutine:
     # refresh the screen
     if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
         win.flip()
-        
- if t >= 0.0 and feedback_3.status == NOT_STARTED:
-           
-            feedback_3.tStart = t
-            feedback_3.frameNStart = frameN  
-            feedback_3.setAutoDraw(True)
-        frameRemains = 0.0 + 1.0- win.monitorFramePeriod
-        core.quit()
-
-        if feedback_3.status == STARTED and t >= frameRemains:
-            feedback_3.setAutoDraw(False)        
-            win.flip()
 
 # -------Ending Routine "thanks"-------
 for thisComponent in thanksComponents:
@@ -823,7 +835,6 @@ for thisComponent in thanksComponents:
         thisComponent.setAutoDraw(False)
 # the Routine "thanks" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
-
 
 
 # these shouldn't be strictly necessary (should auto-save)
